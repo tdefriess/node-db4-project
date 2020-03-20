74e56b2e33d58eec6fd0aa8141beda6ed220617b
Tyler de Friess <tyler.defriess@gmail.com>
2020-03-20 08:21:18 -0400

    in process of seeding, adding field to steps table caused db error

--- data/migrations/20200319213256_bootstrap.js
+++ data/migrations/20200319213256_bootstrap.js
@@ -1,8 +1,33 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('recipes', tbl => {
+        tbl.increments();
+        tbl.string('name', 128).notNullable();
+    })
+    .createTable('ingredients', tbl => {
+        tbl.increments()
+        tbl.string('name', 128).notNullable();
+    })
+    .createTable('steps', tbl => {
+        tbl.increments();
+        tbl.integer('step').unsigned().notNullable();
+        tbl.text('instruction').notNullable();
+        tbl.integer('recipe_id').unsigned().notNullable()
+            .references('id').inTable('recipes')
+            .onUpdate('CASCADE').onDelete('RESTRICT')
+    })
+    .createTable('recipe_ingredients', tbl => {
+        tbl.integer('ingredient_id').unsigned().notNullable()
+            .references('id').inTable('ingredients');
+        tbl.integer('recipe_id').unsigned().notNullable()
+            .references('id').inTable('recipes')
+        tbl.primary(['ingredient_id', 'recipe_id'])
+    })
 };
 
 exports.down = function(knex) {
-  
+    knex.schema.dropTableIfExists('recipe_ingredients')
+        .dropTableIfExists('steps')
+        .dropTableIfExists('ingredients')
+        .dropTableIfExists('recipes')
 };
